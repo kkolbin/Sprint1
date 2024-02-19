1ac401c74d39bed5165fae85e367626e54d95977
kkolbin <126053132+kkolbin@users.noreply.github.com>
2024-02-19 20:14:43 +0000

    Update urls.py

--- Pereval/Pereval/urls.py
+++ Pereval/Pereval/urls.py
@@ -2,19 +2,17 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from new_pereval.views import PassViewSet, EmailView
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 router = routers.DefaultRouter()
-router.register(r'pass', PassViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/submitData/', include(router.urls)),
-    path('', include(router.urls)),
     path('api/submitData/user__email=<str:email>', EmailView.as_view(), name='email'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='docs'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
